acc7d53e76f491d61e4265fb086694b46a613439
mr1-bit <=>
2025-03-12 09:04:33 +0100

    opdracht school

--- opdrachten school/user api op/users.js
+++ opdrachten school/user api op/users.js
@@ -1,3 +1,4 @@
+
 var url  = "https://15euros.nl/api/bier_basic.php";
 
 
@@ -12,5 +13,40 @@ function fLaadBier_fetch() {
         .then(json => {
             console.log("Bier, geladen met AJAX via FETCH API van JS:", json);
             fJson2Html(json, "FETCH API van JS"); // geef JSON en title door aan function
+           
         });
     }
+
+    function fJson2Html(json, title) {
+        document.getElementById("title").innerHTML = title;
+    
+        var table = "<table>";
+        table += "<tr>";
+            table += "<th>";
+                table += "nr";
+            table += "</th>";
+            table += "<th>";
+                table += "naam";
+            table += "</th>";
+            table += "<th>";
+                table += "brouwer";
+            table += "</th>";
+                table += "<th>";
+                table += "inkoop prijs";
+            table += "</th>";
+        table += "</tr>";
+        for(var i=0; i<json.length; i++) {
+            table += "<tr>";
+                table += "<td>" + (i+1) + "</td>";              // zet een volgorde nummer voor in de rij
+                table += "<td>" + json[i].naam + "</td>";       // haal van de array nr i van de json, de object key "naam" op
+                table += "<td>" + json[i].brouwer + "</td>";
+                table += "<td>" + "€ "+  json[i].inkoop_prijs + "</td>";       // haal van de array nr i van de json, de object key "brouwer" op
+            table += "</tr>";
+        }
+        table += "</table>";
+    
+        document.getElementById("title").innerHTML = title; // vul de title die meegegeven is aan de functie in "out_title" in
+        document.getElementById("out_data" ).innerHTML = table; // vul "out_data" met de uit de json opgebouwe table
+    }
+    
+console.log(fLaadBier_fetch())
