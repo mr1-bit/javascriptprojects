7b3b0223ef3a985efb5e27664cb1844f3caeb293
mr1-bit <johnmama60k@gmail.com>
2025-03-18 14:10:28 +0100

    fixed stuff

---
+++ opdrachten school/api2op/jsonhandel.js
@@ -0,0 +1,64 @@
+let users = {
+    users: [
+    {
+    id: 1,
+    name: "Jan Klaassen",
+    iname: "jklaassen",
+    password: "geheim",
+    dob: "2000-01-01",
+    email: "jklaassen@jmeel.nl",
+    id_office: 3
+    },
+    {
+    id: 2,
+    name: "Mark Wilders",
+    iname: "mwilders",
+    password: "geheimer",
+    dob: "1990-10-15",
+    email: "mwilders@jmeel.nl",
+    id_office: 1
+    },
+    {
+    id: 3,
+    name: "Pieter Rutte",
+    iname: "prutte",
+    password: "geheimst",
+    dob: "1981-05-08",
+    email: "prutte@jmeel.nl",
+    id_office: 1
+    },
+    {
+    id: 4,
+    name: "Caroline Omtzigt",
+    iname: "comtzigt",
+    password: "ontzettend",
+    dob: "1980-12-31",
+    email: "comtzigt@jmeel.nl",
+    id_office: 2
+    }
+    ],
+    office: [
+    {
+    id: 1,
+    name: "Administration"
+    },
+    {
+    id: 2,
+    name: "Mangagement"
+    },
+    {
+    id: 3,
+    name: "IT Servicedesk"
+    }
+    ]
+    }
+
+
+let output = document.getElementById("outputDiv")
+
+for (let i = 0; i < users.users.length; i++) {
+    // const el = users.users.id[i];
+    console.log(users.users[i].name)
+}
+
+  
