bc1d9f2c41fceffe05f755d24fe13f85fa24979a
mr1-bit <johnmama60k@gmail.com>
2025-03-18 13:53:09 +0100

    added json from api

--- opdrachten school/user api op/userjson.js
+++ opdrachten school/user api op/userjson.js
@@ -1,30 +1,54 @@
-function calc(a, b, c, d ,e){
-    return a + b + c + d + e;
-}
-jsondarra = {
-    "name": "Darra",
-    "age": 17,
-    "calc": calc(1, 2, 3, 4, 5)
-}
-console.log(jsondarra.calc); // 15
-console.log(jsondarra.name); // Darra
-console.log(jsondarra.age); // 17
-console.log(jsondarra); // { name: 'Darra', age: 17, calc: 15 }
-console.log(jsondarra.calc); // 15
-console.log(jsondarra.age); // 17\
-console.log(jsondarra.name); // Darra
-console.log(jsondarra); // { name: 'Darra', age: 17, calc: 15 }
-console.log(jsondarra.calc); // 15
-console.log(jsondarra.age); // 17
-console.log(jsondarra.name); // Darra
-console.log(jsondarra.calc); // 15
-console.log(jsondarra.name); // Darra
-console.log(jsondarra.age); // 17
-console.log(jsondarra); // { name: 'Darra', age: 17, calc: 15 }
-console.log(jsondarra.calc); // 15
-console.log(jsondarra.age); // 17\
-console.log(jsondarra.name); // Darra
-console.log(jsondarra); // { name: 'Darra', age: 17, calc: 15 }
-console.log(jsondarra.calc); // 15
-console.log(jsondarra.age); // 17
-console.log(jsondarra.name); // Darra
+let users = {
+    users: [
+    {
+    id: 1,
+    name: "Jan Klaassen",
+    iname: "jklaassen",
+    password: "geheim",
+    dob: "2000-01-01",
+    email: "jklaassen@jmeel.nl",
+    id_office: 3
+    },
+    {
+    id: 2,
+    name: "Mark Wilders",
+    iname: "mwilders",
+    password: "geheimer",
+    dob: "1990-10-15",
+    email: "mwilders@jmeel.nl",
+    id_office: 1
+    },
+    {
+    id: 3,
+    name: "Pieter Rutte",
+    iname: "prutte",
+    password: "geheimst",
+    dob: "1981-05-08",
+    email: "prutte@jmeel.nl",
+    id_office: 1
+    },
+    {
+    id: 4,
+    name: "Caroline Omtzigt",
+    iname: "comtzigt",
+    password: "ontzettend",
+    dob: "1980-12-31",
+    email: "comtzigt@jmeel.nl",
+    id_office: 2
+    }
+    ],
+    office: [
+    {
+    id: 1,
+    name: "Administration"
+    },
+    {
+    id: 2,
+    name: "Mangagement"
+    },
+    {
+    id: 3,
+    name: "IT Servicedesk"
+    }
+    ]
+    }
