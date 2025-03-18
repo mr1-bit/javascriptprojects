9bf18e53610727606c7861396f2454761631fd3a
mr1-bit <=>
2025-03-18 13:35:44 +0100

    hallo

---
+++ opdrachten school/user api op/userjson.js
@@ -0,0 +1,15 @@
+function calc(a, b, c, d ,e){
+    return a + b + c + d + e;
+}
+jsondarra = {
+    "name": "Darra",
+    "age": 17,
+    "calc": calc(1, 2, 3, 4, 5)
+}
+console.log(jsondarra.calc); // 15
+console.log(jsondarra.name); // Darra
+console.log(jsondarra.age); // 17
+console.log(jsondarra); // { name: 'Darra', age: 17, calc: 15 }
+console.log(jsondarra.calc); // 15
+console.log(jsondarra.age); // 17\
+console.log(jsondarra.name); // Darra
